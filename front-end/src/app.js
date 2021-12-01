d03a53ac033d2dfbf1934272d969feff8eddf8f2
Andrew <andrewunifei@gmail.com>
2021-12-01 12:51:12 -0300

    Ajustes

--- front-end/src/app.js
+++ front-end/src/app.js
@@ -66,7 +66,7 @@ $(window).on('load', function(){
         metamask_modal = document.getElementById("metamask-modal");
 
         if(ethereum.selectedAddress == null){
-            metamask_modal.style.display = 'none';
+            metamask_modal.style.display = 'block';
         }
         else{
             metamask_modal.style.display = 'none';
